29703b5b09ad6bc2ef30490985940e4db89d68ef
dantenol <dnolascodante@gmail.com>
2022-11-15 14:16:34 -0300

    done exercises

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,36 @@
 
 print("\n******************* Python Calculator *******************")
 
+print("Qual operação você deseja realizar?")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
 
+opcao = int(input("Digite a opção desejada (1/2/3/4): "))
+
+if (opcao < 1 or opcao > 4):
+    print("Opção inválida!")
+    quit()
+
+num1 = int(input('Digite o primeiro número: '))
+
+num2 = int(input('Digite o segundo número: '))
+
+sum = lambda num1, num2: num1 + num2
+sub = lambda num1, num2: num1 - num2
+mult = lambda num1, num2: num1 * num2
+div = lambda num1, num2: num1 / num2
+resultado = 0
+if opcao == 1:
+   resultado = sum(num1, num2)
+elif opcao == 2:
+    resultado = sub(num1, num2)
+elif opcao == 3:  
+    resultado = mult(num1, num2)
+elif opcao == 4:
+    resultado = div(num1, num2)
+else:
+    print('Opção inválida!')
+    quit()
+print('Resultado: %s' % resultado)
